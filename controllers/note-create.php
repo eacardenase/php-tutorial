a46e1326b05feb4b50718f7cddd835d06fcd095e
Edwin Cardenas <eacardenase@unal.edu.co>
2023-08-24 19:37:10 -0500

    refactor: extract validation logic into separate class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,19 +1,18 @@
 <?php
 
+require('Validator.php');
+$config = require('config.php');
+
 $heading = 'Create Note';
 
-$config = require('config.php');
+
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'A body is required.';
-    }
-
-    if (strlen($_POST['body']) >= 1000) {
-        $errors['body'] = 'A note length could not be greater than 1,000 characters.';
+    if (!Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
     }
 
     if (empty($errors)) {
