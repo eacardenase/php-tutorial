c47aadf584f450e97b788639d7d0545a02d7f59e
Edwin Cardenas <eacardenase@unal.edu.co>
2023-09-07 21:19:06 -0500

    feat: add delete note by id option

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,16 +5,35 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$id = $_GET['id'];
-$currentUserId = 1;
+$currentUserId = 10;
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    'id' => $id
-])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
 
-authorize($note['user_id'] === $currentUserId);
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', [
+        'id' => $id
+    ])->findOrFail();
 
-view('notes/show.view.php', [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('DELETE FROM notes WHERE id = :id', [
+        'id' => $id
+    ]);
+
+    header('location: /notes');
+
+    exit();
+} else {
+    $id = $_GET['id'];
+
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', [
+        'id' => $id
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view('notes/show.view.php', [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -12,6 +12,12 @@
         <p class="text-blue-400">
             <?= htmlspecialchars($note['body']) ?>
         </p>
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+            <button class="text-sm text-red-500">
+                Delete
+            </button>
+        </form>
     </div>
 </main>
 
