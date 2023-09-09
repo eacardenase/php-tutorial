9b7931e68c0440d21c84618fdf5377067328ee50
Edwin Cardenas <eacardenase@unal.edu.co>
2023-09-08 19:14:37 -0500

    refactor: extract notes post request into its own controller

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -6,7 +6,7 @@ $config = require base_path('config.php');
 $db = new Database($config['database']);
 
 $currentUserId = 1;
-$id = $_GET['id'];
+$id = $_POST['id'];
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
     'id' => $id
@@ -14,7 +14,10 @@ $note = $db->query('SELECT * FROM notes WHERE id = :id', [
 
 authorize($note['user_id'] === $currentUserId);
 
-view('notes/show.view.php', [
-    'heading' => 'Note',
-    'note' => $note
+$db->query('DELETE FROM notes WHERE id = :id', [
+    'id' => $id
 ]);
+
+header('location: /notes');
+
+exit();
