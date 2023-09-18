f6ec93ff94cc3072f8d3156e2799e92cab5499f8
Edwin Cardenas <eacardenase@unal.edu.co>
2023-09-18 12:44:36 -0500

    feat: add update note functionality

---
+++ controllers/notes/update.php
@@ -0,0 +1,42 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+$id = $_POST['id'];
+$body = $_POST['body'];
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    'id' => $id
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$errors = [];
+
+if (!Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required.';
+}
+
+if (count($errors)) {
+    view('notes/edit.view.php', [
+        'heading' => 'Edit Note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+
+    return;
+}
+
+$db->query('UPDATE notes SET body = :body WHERE id = :id', [
+    'id' => $id,
+    'body' => $body
+]);
+
+header('location: /notes');
+
+exit();
