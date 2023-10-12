18080792194990b7463349da1d6ee18254ad2418
Edwin Cardenas <eacardenase@unal.edu.co>
2023-10-12 12:44:41 -0500

    feat: add register form and controller

--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,5 @@
 <?php
 
-$_SESSION['name'] = 'Edwin';
-
 view('index.view.php', [
     'heading' => 'Home'
 ]);
---
+++ controllers/registration/store.php
@@ -0,0 +1,49 @@
+<?php
+
+use Core\Validator;
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+
+if (!Validator::email($email)) {
+    $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (!Validator::string($password, 6, 16)) {
+    $errors['password'] = 'Please provide a password of at least 7 characters.';
+}
+
+if (! empty($errors)) {
+    view('registration/create.view.php', [
+       "errors" => $errors
+    ]);
+
+    return;
+}
+
+$user = $db->query('SELECT * FROM users WHERE email = :email', [
+    'email' => $email
+])->findOne ();
+
+if ($user) {
+    header('location: /');
+    exit();
+} else {
+    $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
+        'email' => $email,
+        'password' => $password
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+
+    header('location: /');
+    exit();
+}
