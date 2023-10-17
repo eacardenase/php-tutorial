eff21bf959180d33c43ca708c31a4749a611da77
Edwin Cardenas <eacardenase@unal.edu.co>
2023-10-17 09:03:53 -0500

    refactor: extract login form validation into its own class

---
+++ Http/Forms/LoginForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Http\Forms;
+
+use Core\Validator;
+
+class LoginForm
+{
+    protected array $errors = [];
+
+    public function validate(string $email, string $password): bool
+    {
+        if (!Validator::email($email)) {
+            $this->errors['email'] = 'Please provide a valid email address.';
+        }
+
+        if (!Validator::string($password)) {
+            $this->errors['password'] = 'Please provide a valid password.';
+        }
+
+        return empty($this->errors);
+    }
+
+    public function errors(): array
+    {
+        return $this->errors;
+    }
+}
--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -2,29 +2,21 @@
 
 use Core\App;
 use Core\Database;
-use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
+$form = new LoginForm();
 
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please provide a valid email address.';
-}
-
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password.';
-}
-
-if (!empty($errors)) {
+if (!$form->validate($email, $password)) {
     view('session/create.view.php', [
-        "errors" => $errors
+        "errors" => $form->errors()
     ]);
 
-    return;
+    exit();
 }
 
 $user = $db->query('SELECT * FROM users WHERE email = :email', [
